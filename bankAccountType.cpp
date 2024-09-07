ebebf58f7bd6ce66a1bf53806cdad2266b572cd3
Tartanduck <crainxc@gmail.com>
2024-09-07 12:13:40 -0700

    Update bankAccountType.cpp

--- bankAccountType.cpp
+++ bankAccountType.cpp
@@ -1,51 +1,52 @@
-#include "header.h"
+//implementation file
+
+#include <iostream>
+#include <string>
+#include <iomanip>
 #include "bankAccountType.h"
 
 using namespace std;
 
 bankAccountType::bankAccountType(string n, int acctNumber, double bal)
 {
-	name = n;
-	accountNumber = acctNumber;
-	balance = bal;
+        name = n;
+        accountNumber = acctNumber;
+        balance = bal;
 }
 
 int bankAccountType::getAccountNumber()
 {
-	return accountNumber;
-}	
+        return accountNumber;
+}
 
 double bankAccountType::getBalance()
 {
-	return balance;
+        return balance;
 }
 
 string bankAccountType::getName()
 {
-	return name;
+        return name;
 }
 
 void bankAccountType::setName(string n)
 {
-	name = n;
+        name = n;
 }
 
-void bankAccountType::withdraw(double amount) //don't put virtual in the .cpp file
+void bankAccountType::withdraw(double amount)
 {
-	balance = balance - amount;
-
+        balance = balance - amount;
 }
 
 void bankAccountType::deposit(double amount)
 {
-		balance = balance + amount;
+        balance = balance + amount;
 }
 
 void bankAccountType::print()
 {
-	cout << fixed << showpoint << setprecision (2);
-	cout << name << " " << accountNumber << "balance : $ " << balance;
-
+        cout << fixed << showpoint << setprecision(2);
+        cout << name << " " << accountNumber << " balance: $"
+                        << balance;
 }
-
-//there is no create montly statement making it a pure virtual function
