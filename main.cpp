a911f8d5c9499c4c1dae00b35905c7dd6e230e9a
Tartanduck <crainxc@gmail.com>
2024-09-07 12:21:15 -0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,92 +1,112 @@
-#include "bankAccountType.h"
 #include "header.h"
-#include "savingsAccountType.h"
-
-#include "serviceChargeCheckingType.h"
-#include "highInterestCheckingType.h"
-#include "highInterestSavingsType.h"
-
-#include "certificationOfDepositType.h"
-#include "checkingAccountType.h"
-
-#include "testFunction.h"
 
 int main()
 {
-	//variables
-	int elements = 0;
-	int index = 0;
-	vector<bankAccountType *> accountList;
+        vector<bankAccountType *> accountsList;
+        
+         cout << "\033c";
+                char userChoice = 0;
+                do  {
+                   printMainMenu();
+                   cout  << "Enter your choice: " << endl;
+                        cin  >> userChoice;
 
-	//test fill classes
-	accountList.push_back(new savingsAccountType("Bill", 10200, 2500, 52.2));
+                        //fixes input failure and does not allow the "enter correct choice" screen to repeat
+                        while (userChoice < 49 || userChoice > 51 ) {
+                                cout << "\033c";
+                                printMainMenu();
+                                cout << "Please enter a correct choice" << endl << endl;
+                                cout << "Enter your choice:"  << endl;
+                                cin  >> userChoice;
+                        }
+                        cout << "\033c";
+                        switch (userChoice) {
+                                case '1':
+                                        account(accountsList);
+                                        break;
+                                case '2':
+                                        cout << "Receipt Menu" << endl;
+                                        //receipt();
+                                        break;
+                                case '3':
+                                        break;
+                                }
+                        cout << endl;
+                } while (userChoice != 51);
 
-	accountList.push_back(new savingsAccountType("Bill", 10200, 2500));
-
-   accountList.push_back(new highInterestCheckingType("Bill", 10200, 2500));
-
-	accountList.push_back(new noServiceChargeCheckingType("Joe" , 12000, 1500));
-
-	accountList.push_back(new serviceChargeCheckingType("James" , 12000, 1500));
-
-//	accountList.push_back(new highInterestSavingsType("Colin" , 12000, 4500.0, 500.0, "Test"));
-
-//	highInterestSavingsType highList("Name", 12345, 1000.0);
- 
-	 highInterestSavingsType List;
-	
-	//test();
-	//get elements
-	elements = accountList.size();
-//	cout << "your elements:" << elements << endl;
-//	cin.ignore(10000 , '\n');
-
-	if(elements > 0)
-	{
-		while( elements > index)
-		{
-			accountList[index]->print();
-			index++;	
-//			cout << "index:" << index << endl;
-//			cin.ignore(1000000 , '\n');
-		}
-	}
-
-//	delete highList;	
-
-//	yes();
-//	testFunctionType();
-//dd
-//	delete [] accountList;
-
-//	cout << " Hello world " << endl;
-
-	//clear variables
-//	studentType* studentPtr = nullptr;
-	
-	
-	//saves the pointer 
-//	studentPtr =  new studentType;
-
-//	(*studentPtr).gpa = 3.9;
-//	studentPtr->gpa = 3.9;
-
-//	cout << "Your gpa is " << studentPtr->gpa << endl;
-
-	
-
-	
-	
-
-//	int *ptrNum = nullptr ;
-//	int num =  25;
-
-//	ptrNum = &num;
-
-//	cout << " Your number is " << *ptrNum << endl;
-//	cout << " Your number address is " << ptrNum << endl;
-
-//	delete studentPtr;
-
-	return 0;
+                return 0;
 }
+
+
+void printMainMenu() {
+                cout << "Main Menu" << endl;
+                cout << "1. Account" << endl;
+                cout << "2. Receipt" << endl;
+                cout << "3. Exit" << endl;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        
+        
+        // accountsList.push_back(new savingsAccountType("Bill", 10200, 2500));
+        // accountsList.push_back(new highInterestSavingsType("Susan", 10210, 2000));
+        // accountsList.push_back(new noServiceChargeCheckingType("John", 20100, 3500));
+        // accountsList.push_back(new serviceChargeCheckingType("Ravi", 30100, 1800));
+        // accountsList.push_back(new highInterestCheckingType("Sheila", 20200, 6000));
+        // accountsList.push_back(new certificateOfDepositType("Hamid", 51001, 18000, 0.075, 18));
+
+
+        //in order for this to work I had to change the for loops from (int i = 0;) to the (vector<bankAccountType*>::size_type i = 0;)
+
+    //     cout << "January:\n-------------" << endl;
+    //     for (vector<bankAccountType*>::size_type i = 0; i < accountsList.size(); i++)
+    //     {
+    //             accountsList[i]->createMonthlyStatement();
+    //             accountsList[i]->print();
+    //             cout << endl;
+    // }
+
+    //     cout << "\nFebruary:\n-------------" << endl;
+    //     for (vector<bankAccountType*>::size_type i = 0; i < accountsList.size(); i++)
+    //     {
+    //             accountsList[i]->createMonthlyStatement();
+    //             accountsList[i]->print();
+    //             cout << endl;
+    //     }
+
+    //     for (vector<bankAccountType*>::size_type i = 0; i < accountsList.size(); i++)
+    //     {
+    //             accountsList[i]->withdraw(500);
+    //     }
+
+    //     cout << "\nMarch:\n-------------" << endl;
+    //     for (vector<bankAccountType*>::size_type i = 0; i < accountsList.size(); i++)
+    //     {
+    //             accountsList[i]->createMonthlyStatement();
+    //             accountsList[i]->print();
+    //             cout << endl;
+    //     }
