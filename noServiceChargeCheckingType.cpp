acb81acfd406fa2fa6d8eb60629892a8bc16a077
Tartanduck <crainxc@gmail.com>
2024-09-07 12:21:58 -0700

    Update noServiceChargeCheckingType.cpp

--- noServiceChargeCheckingType.cpp
+++ noServiceChargeCheckingType.cpp
@@ -1,3 +1,4 @@
+//implementation file
 
 #include <iostream>
 #include <string>
@@ -10,46 +11,45 @@ const double noServiceChargeCheckingType::MIN_BALANCE = 1000.00;
 const double noServiceChargeCheckingType::INTEREST_RATE = 0.02;
 
 noServiceChargeCheckingType::noServiceChargeCheckingType(string n, int acctNumber, double bal)
-									:checkingAccountType(n, acctNumber, bal)
+                                                                        : checkingAccountType(n, acctNumber, bal)
 {
-	minimumBalance = MIN_BALANCE;
-	interestRate   = INTEREST_RATE;
-
+        minimumBalance = MIN_BALANCE;
+        interestRate = INTEREST_RATE;
 }
 
-noServiceChargeCheckingType::noServiceChargeCheckingType(string n, int acctNumber, double bal, double minBalance, double intRate)
-									:checkingAccountType(n, acctNumber, bal)
+noServiceChargeCheckingType::noServiceChargeCheckingType(string n, int acctNumber, double bal,
+                                                                                                                       double minBalance, double intRate)
+                                                                        : checkingAccountType(n, acctNumber, bal)
 {
-	minimumBalance = minBalance;
-	interestRate   = intRate;
-
+        minimumBalance = minBalance;
+        interestRate = intRate;
 }
 
 double noServiceChargeCheckingType::getMinimumBalance()
 {
-	return minimumBalance;
+        return minimumBalance;
 }
 
 void noServiceChargeCheckingType::setMinimumBalance(double minBalance)
 {
-	minimumBalance = minBalance;
+        minimumBalance = minBalance;
 }
 
-bool noServiceChargeCheckingType::verifyMinimumumBalance(double amount)
+bool noServiceChargeCheckingType::verifyMinimumBalance(double amount)
 {
-	return (balance - amount >= minimumBalance);
+        return (balance - amount >= minimumBalance);
 }
 
 void noServiceChargeCheckingType::writeCheck(double amount)
 {
-	if(verifyMinimumumBalance(amount))
-		balance = balance - amount;
+        if (verifyMinimumBalance(amount))
+                balance = balance - amount;
 }
 
 void noServiceChargeCheckingType::withdraw(double amount)
 {
-	if(verifyMinimumumBalance(amount))
-		balance = balance - amount;
+        if (verifyMinimumBalance(amount))
+                balance = balance - amount;
 }
 
 void noServiceChargeCheckingType::createMonthlyStatement()
@@ -58,9 +58,7 @@ void noServiceChargeCheckingType::createMonthlyStatement()
 
 void noServiceChargeCheckingType::print()
 {
-	cout << fixed << showpoint << setprecision(2);
-	cout << "No Service Charge Check. " << getName() << "\t ACCT# "
-		  << getAccountNumber() << "\tBalance: $" << getBalance() << endl;
+        cout << fixed << showpoint << setprecision(2);
+        cout << "No Service Charge Check: " << getName() << "\t ACCTN# "
+                        << getAccountNumber() << "\tBalance: $" << getBalance();
 }
-
-
