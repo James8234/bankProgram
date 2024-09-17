813993e312e892359842dafa0b93b6dc938f3aaf
zhernandez11 <144408706+zhernandez11@users.noreply.github.com>
2024-09-17 09:46:54 -0700

    login.h

---
+++ login.h
@@ -0,0 +1,25 @@
+#ifndef LOGIN_H
+#define LOGIN_H
+
+#include<string>
+#include<iostream>
+#include<vector>
+
+using namespace std;
+
+class User
+{
+public:
+    string username;
+    string password;
+    int id;
+
+    User(const string& usr, const string& pswd, int id);
+
+    void displayLoginMenu(vector<User>& users);
+
+   //bool validLogin(const string& usr, const string& pswd, int id);
+};
+
+
+#endif
